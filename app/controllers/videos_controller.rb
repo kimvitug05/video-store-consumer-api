52eece577c0f8026f2fa5c4bb1b029600293ca7d
Kimberly Vitug <kim_vitug05@yahoo.com>
2021-01-21 12:28:11 -0800

    add create video endpoint

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -21,6 +21,20 @@ class VideosController < ApplicationController
       )
   end
 
+  def create
+    new_video = Video.new(external_id: video_params[:external_id], title: video_params[:title],overview: video_params[:overview], release_date: video_params[:release_date], image_url: video_params[:image_url], inventory: 7 )
+
+    if new_video.external_id != nil && !Video.find_by(external_id: new_video.external_id)
+      if new_video.save
+        render status: :created, json: {}
+      else
+        render status: :bad_request, json: { errors: video.errors.messages }
+      end
+    else
+      render status: :bad_request, json: { errors: "Video already in database" }
+    end
+  end
+
   private
 
   def require_video
@@ -28,5 +42,9 @@ class VideosController < ApplicationController
     unless @video
       render status: :not_found, json: { errors: { title: ["No video with title #{params["title"]}"] } }
     end
+  end
+
+  def video_params
+    return params.permit(:external_id, :title, :inventory, :overview, :release_date, :image_url)
   end
 end
